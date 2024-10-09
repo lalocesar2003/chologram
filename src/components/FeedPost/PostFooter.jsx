2390ad983df002943bce6f6f62b632b86282257e
lalocesar2003 <ceed1000@gmail.com>
2024-10-09 12:49:43 -0500

    1 hora con 21 minutos

--- src/components/FeedPost/PostFooter.jsx
+++ src/components/FeedPost/PostFooter.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import {
+  Box,
+  Flex,
+  Text,
+  InputGroup,
+  Button,
+  Input,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { NotificationsLogo, UnlikeLogo } from "../../assets/constants";
+import { CommentLogo } from "../../assets/constants";
 
 const PostFooter = () => {
-  return <div>PostFooter</div>;
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(1000);
+
+  const handleLike = () => {
+    if (liked) {
+      setLiked(false);
+      setLikes(likes - 1);
+    } else {
+      setLiked(true);
+      setLikes(likes + 1);
+    }
+  };
+
+  return (
+    <>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
+        <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
+          {liked ? <NotificationsLogo /> : <UnlikeLogo />}
+        </Box>
+
+        <Box cursor={"pointer"} fontSize={18}>
+          <CommentLogo />
+        </Box>
+      </Flex>
+      <Text fontWeight={600} fontSize={"sm"}>
+        {likes} likes
+      </Text>
+      <Text fontSize="sm" fontWeight={700}>
+        asapprogrammer{" "}
+        <Text as="span" fontWeight={400}>
+          Feeling good
+        </Text>
+      </Text>
+      <InputGroup>
+        <Input
+          variant={"flushed"}
+          placeholder={"Add a comment..."}
+          fontSize={14}
+        />
+        <InputRightElement>
+          <Button
+            fontSize={14}
+            color={"blue.500"}
+            fontWeight={600}
+            cursor={"pointer"}
+            _hover={{ color: "white" }}
+            bg={"transparent"}
+          >
+            Post
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    </>
+  );
 };
 
 export default PostFooter;
