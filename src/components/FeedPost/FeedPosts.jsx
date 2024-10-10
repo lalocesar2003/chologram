d5705cbfb37d587168e65c126e5bf7f3f704510d
lalocesar2003 <ceed1000@gmail.com>
2024-10-10 09:58:39 -0500

    1 hora con 48 minutos

--- src/components/FeedPost/FeedPosts.jsx
+++ src/components/FeedPost/FeedPosts.jsx
@@ -1,13 +1,49 @@
-import { Container } from "@chakra-ui/react";
+import {
+  Container,
+  Skeleton,
+  SkeletonCircle,
+  VStack,
+  Box,
+} from "@chakra-ui/react";
 import FeedPost from "./FeedPost";
+import { useEffect, useState } from "react";
 
 const FeedPosts = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <Container maxW={"container.sm"} py={10} px={2}>
-      <FeedPost img="/img1.png" username="burakorkmezz" avatar="/img1.png" />
-      <FeedPost img="/img2.png" username="josh" avatar="/img2.png" />
-      <FeedPost img="/img3.png" username="janedoe" avatar="/img3.png" />
-      <FeedPost img="/img4.png" username="johndoe" avatar="/img4.png" />
+    <Container maxW="container.sm" py={10} px={2}>
+      {isLoading &&
+        [1, 2, 3].map((_, idx) => (
+          <VStack key={idx} gap={4} alignItems="flex-start" mb={10}>
+            <SkeletonCircle size="10" />
+            <VStack gap={2} alignItems="flex-start">
+              <Skeleton height="10px" w="200px" />
+              <Skeleton height="10px" w="200px" />
+            </VStack>
+            <Skeleton w="full">
+              <Box h="500px">contents wrapped</Box>
+            </Skeleton>
+          </VStack>
+        ))}
+      {!isLoading && (
+        <>
+          <FeedPost
+            img="/img1.png"
+            username="burakorkmezz"
+            avatar="/img1.png"
+          />
+          <FeedPost img="/img2.png" username="josh" avatar="/img2.png" />
+          <FeedPost img="/img3.png" username="janedoe" avatar="/img3.png" />
+          <FeedPost img="/img4.png" username="johndoe" avatar="/img4.png" />
+        </>
+      )}
     </Container>
   );
 };
---
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -0,0 +1,5 @@
+const SuggestedUser = () => {
+  return <div></div>;
+};
+
+export default SuggestedUser;
